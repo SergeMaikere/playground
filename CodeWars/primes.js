1adbe0ccbfa1fc51272f000ef872686cdd3c90fb
SergeMaikere <serge.maikere@gmail.com>
2025-02-07 17:29:10 +0100

    remove useless function calls

--- CodeWars/primes.js
+++ CodeWars/primes.js
@@ -2,21 +2,56 @@
 class Primes {
 
 	static * stream () {
+
 		const MAX = 472882028
-		const LIMIT = Math.floor(Math.sqrt(MAX))
-		const RANGE = Array(LIMIT+1).fill(true)
-		const PRIMES = this.getCorePrimes(RANGE, LIMIT)
+		const LIMIT = Math.floor(Math.sqrt(MAX))+1
+		const RANGE = new Array(LIMIT+1).fill(true)
 
 		let start = 0
 		let top = LIMIT
+		let f, p, sieve
+
+		sieve = [...RANGE]
+		sieve.fill(false, 0, 2)
+
+		for (let p = 2; p*p < LIMIT; p++) {
+			if (sieve[p]) {
+				for (let i = p*p; i <= LIMIT; i+=p) {
+					sieve[i] = false
+				}
+			}
+		}
+		
+		let PRIMES = []
+		for (let i = 2; i < LIMIT; i++) {
+			if (sieve[i]) PRIMES.push(i)
+		}
+
 		let currPrimes = PRIMES
 
-		while (start <= MAX) {
+		while (start < MAX) {
 
 			if (top > MAX) top = MAX
 
 			if (start >= LIMIT) { 
-				currPrimes = this.getSegmentedSievePrimes(start, top, [...RANGE], PRIMES, LIMIT)
+				sieve = [...RANGE]
+
+				const len = PRIMES.length
+				for (let i = 0; i < len; i++) {
+					p = PRIMES[i]
+
+					f = Math.floor( start / p ) * p
+					if ( f < start ) f += p
+
+					for (let j = f; j < top; j+=p) {
+						sieve[j - start] = false
+					}
+				}
+
+				currPrimes = []
+				for (let i = 0; i < LIMIT; i++) {
+					if (sieve[i]) currPrimes.push(i + start)
+				}
 			}
 
 			const len = currPrimes.length
@@ -28,50 +63,6 @@ class Primes {
 			top += LIMIT
 		}
 
-	}
-
-	static getCorePrimes (range, limit) { 
-		let temp = [...range]
-		temp.fill(false, 0, 2)
-
-		for (let p = 2; p*p < limit; p++) {
-			if (temp[p]) {
-				for (let i = p*p; i <= limit; i+=p) {
-					temp[i] = false
-				}
-			}
-		}
-		return this.filterMultiples(0, temp, limit)
-	}
-
-	static getSegmentedSievePrimes (start, top, sieve, primes, limit) {
-		let f, p
-
-		const len = primes.length
-		for (let i = 0; i < len; i++) {
-			p = primes[i]
-			
-			f = this.getFirstMultiple(start, p)
-
-			for (let j = f; j <= top; j+=p) {
-				sieve[j - start] = false
-			}
-		}
-		return this.filterMultiples(start, sieve, limit)
-	}
-
-	static filterMultiples (offset, sieve, limit) {
-		let result = []
-		for (let i = 0; i < limit; i++) {
-			if (sieve[i]) result.push(i + offset)
-		}
-		return result
-	}
-
-	static getFirstMultiple (start, prime) {
-		let result = Math.floor( start / prime ) * prime
-		if ( result < start ) result += prime
-		return result
 	}
 }
 
