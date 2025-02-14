0bb068a2b9ee6106fc555eee98bc75379b9c86c1
SergeMaikere <serge.maikere@gmail.com>
2025-02-14 15:53:08 +0100

    move incremental sieve

--- CodeWars/primes.js
+++ CodeWars/primes.js
@@ -3,71 +3,39 @@ class Primes {
 
 	static * stream () {
 
-		const MAX = 472882028
-		const LIMIT = Math.floor(Math.sqrt(MAX))+1
-		const RANGE = [...new Array(LIMIT+1)].map( (_n, i) => i )
-		const PRIMES = this.getCoreMultiples( [...RANGE], LIMIT )
+		let multies = new Map()
+		let primes, nextM, p
+		let n = 2
 
-		let currPrimes = PRIMES
-		let start = 0
-		let top = LIMIT
-		let f, p, tempSet
-		
-		while (start < MAX) {
+		while (true) {
+			if ( !multies.has(n) ) {
+				yield n
+				multies.set(n*n, [n])
+			}
+			else {
+				primes = multies.get(n)
 
-			if (top > MAX) top = MAX
+				for (let i = 0; i < primes.length; i++) {
+					p = primes[i]
+					nextM = p + n
 
-			if (start >= LIMIT) {
-				tempSet = new Set( [...RANGE].map(n => n + start) )
-
-				for (const p of PRIMES.values()) {
-
-					f = Math.floor( start / p ) * p
-					if ( f < start ) f += p
-
-					for (let j = f; j < top; j+=p) {
-						tempSet.delete(j)
-					}
+					multies.has(nextM) ? multies.get(nextM).push(p) : multies.set(nextM, [p])
 				}
-						
-				currPrimes = tempSet
+
+				multies.delete(n)
 			}
-
-			for (const n of currPrimes.values()) yield n
-
-			start += LIMIT
-			top += LIMIT
+			n++
 		}
-	}
-
-	static getCoreMultiples (range, limit) {
-		let primes = new Set( range.slice(2) )
-
-		for (let p = 2; p*p < limit; p++) {
-			for (let i = p*p; i <= limit; i+=p) {
-				primes.delete(i)
-			}
-		}
-		return primes
 	}
 }
 
-
-const deliverPrimes = n => {
-	const stream = Primes.stream()
-	for (let i = 0; i < n; i++) {
-		stream.next()
-	}
-}
-
-const deliverPrimesVerbose = n => {
-	const stream = Primes.stream()
+const displayPrimes = n => {
 	let src = ''
+	const stream = Primes.stream()
 	for (let i = 0; i < n; i++) {
-		src += ` | ${stream.next().value}`
+		stream.next().value
 	}
 	console.log(src)
 }
 
-// deliverPrimesVerbose(3000)
-deliverPrimes(25e6)
+displayPrimes(1e6)
