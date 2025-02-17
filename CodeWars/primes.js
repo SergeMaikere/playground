d3158ae2867e2abef2b6586307ca06d5acfc39a1
SergeMaikere <serge.maikere@gmail.com>
2025-02-17 20:54:56 +0100

    basic sieve of atkin

--- CodeWars/primes.js
+++ CodeWars/primes.js
@@ -1,41 +1,63 @@
-
 class Primes {
 
 	static * stream () {
 
-		let multies = new Map()
-		let primes, nextM, p
-		let n = 2
+		const MAX = 1000
+		const SQRT = Math.floor( Math.sqrt(MAX) ) + 1
+		const RANGE = Array( MAX ).fill(false).fill(true, 2, 4)
 
-		while (true) {
-			if ( !multies.has(n) ) {
-				yield n
-				multies.set(n*n, [n])
+		let primes = []
+		let sieve = [...RANGE]
+
+		for (let x = 1; x < SQRT; x++) {
+			for (let y = 1; y < SQRT; y++) {
+				this.#findPrimesAtkinWay( sieve, MAX, x, y )
 			}
-			else {
-				primes = multies.get(n)
+		}
 
-				for (let i = 0; i < primes.length; i++) {
-					p = primes[i]
-					nextM = p + n
 
-					multies.has(nextM) ? multies.get(nextM).push(p) : multies.set(nextM, [p])
+		for (let i = 5; i < SQRT; i++) {
+			if ( sieve[i] ) {
+				let s = i*i
+				for (var j = s; j < MAX; j+=s) {
+					sieve[j] = false
 				}
-
-				multies.delete(n)
 			}
-			n++
+		}
+
+		for (let k = 0; k < MAX; k++) {
+			if ( sieve[k] ) primes.push(k)
+		}
+
+		for (let p of primes) yield p
+	}
+
+	static #findPrimesAtkinWay = (sieve, max, x, y) => {
+
+		let n = 4 * x * x + y * y
+		if ( (n < max) && ((n % 12 == 1) || (n % 12 == 5)) ) {
+			sieve[n] = true
+		}
+
+		n = 3 * x * x + y * y
+		if ( (n < max) && (n % 12 == 7) ) {
+			sieve[n] = true
+		}
+
+		n = 3 * x * x - y * y
+		if ( (x > y) && (n < max) && (n % 12 == 11) ) {
+			sieve[n] = true
 		}
 	}
 }
 
-const displayPrimes = n => {
+const displayPrimes = (n) => {
 	let src = ''
 	const stream = Primes.stream()
 	for (let i = 0; i < n; i++) {
-		stream.next().value
+		src += `| ${stream.next().value}`
 	}
-	console.log(src)
+	console.log({src})
 }
 
-displayPrimes(1e6)
+displayPrimes(100)
