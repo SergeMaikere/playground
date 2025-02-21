32c26e2c3552a03882d14b4fe31c94accad13b7b
SergeMaikere <serge.maikere@gmail.com>
2025-02-21 20:38:14 +0100

    segmented sieve eratosthenes final form

--- CodeWars/primes.js
+++ CodeWars/primes.js
@@ -5,81 +5,78 @@ class Primes {
 
 		const MAX = 472882027
 		const LIMIT = Math.floor(Math.sqrt(MAX)) + 1
-		const RANGE = 1e4
-		const PRIMES = this.#getCorePrimes(MAX)
+		const RANGE = 15e3
+		const SIEVE = Array(RANGE + 1).fill (true)
+		const PRIMES = this.#getCorePrimes(LIMIT)
 
-		for ( let p of PRIMES ) yield p
+		let pLen = PRIMES.length
+		for (let m = 0; m < pLen; m++) {
+			yield PRIMES[m]
+		}
 
-		let currPrimes, newSieve, processedSieve, p
+		let currPrimes, newSieve, cLen, f, p
 
 		let start = LIMIT
 		let top = LIMIT + RANGE
 
 		while (true) {
+			newSieve = [...SIEVE]
 
-			newSieve = this.#getNewSieve(start, RANGE)
-			processedSieve = this.#getSegmentedSieve(PRIMES, newSieve, start, top)
-			currPrimes = this.#filterSieve(processedSieve, start, RANGE)
+			for (let i = 0; i < pLen; i++) {
+				p = PRIMES[i]
 
-			for ( let p of currPrimes ) yield p
+				f = Math.floor(start / p) * p
+				if ( f < start ) f += p
+
+				for (let j = f; j <= top; j+=p) {
+					newSieve[j - start] = false
+				}
+			}
+
+			currPrimes = []
+			for (let k = 0; k < RANGE; k++) {
+				if ( newSieve[k] ) currPrimes.push(k + start)
+			}
+
+			cLen = currPrimes.length
+			for (let l = 0; l < cLen; l++) {
+				yield currPrimes[l]
+			}
 
 			start += RANGE
 			top += RANGE
 		}		
 	}
 
-	static #getCorePrimes = (max) => {
-		let limit = Math.floor(Math.sqrt(max)) + 1
-		let sieve = Array(limit + 1).fill(true).fill(false, 0, 2)
+	static #getCorePrimes = (limit) => {
+		let newSieve = Array(limit + 1).fill(true).fill(false, 0, 2)
 
 		for (let i = 2; i*i < limit; i++) {
-			if ( sieve[i] ) {
+			if ( newSieve[i] ) {
 				for (let j = i*i; j <= limit; j+=i) {
-					sieve[j] = false
+					newSieve[j] = false
 				}
 			}
 		}
-		return this.#filterSieve(sieve, 0, limit)
-	}
-
-	static #getSegmentedSieve = (primes, sieve, start, top) => {
-		let len = primes.length
-		for (let i = 0; i < len; i++) {
-			let f = this.#getFirstMultipleOfPrime(primes[i], start)
-
-			for (let j = f; j <= top; j+=primes[i]) {
-				sieve[j - start] = false
-			}
-		}
-		return sieve
-	}
-
-	static #filterSieve = (sieve, start, limit) => {
+		
 		let primes = []
-		for (let l = 0; l < limit; l++) {
-			if ( sieve[l] ) primes.push(l + start)
+		for (let k = 0; k < limit; k++) {
+			if ( newSieve[k] ) primes.push(k)
 		}
 		return primes
 	}
-
-	static #getFirstMultipleOfPrime = (prime, start) => {
-		let f = Math.floor(start / prime) * prime
-		if ( f <= start ) f += prime
-		return f
-	}
-
-	static #getNewSieve = (start, size) => start == 0 ? Array(size).fill(true).fill(false, 0, 2) : Array(size).fill(true)
 }
 
 const display = n => {
 	let src = ''
 	const stream = Primes.stream()
 	for (let i = 0; i < n; i++) {
-		src = `| ${stream.next().value}`
+		// src = `| ${stream.next().value}`
+		stream.next().value
 	}
-	console.log({src})
+	// console.log({src})
 }
 
-display(2440)
+display(25e6)
 
 
