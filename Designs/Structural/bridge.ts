6747b6f56e24079c4c61652de21397caf5289bb0
SergeMaikere <serge.maikere@gmail.com>
2025-03-31 14:40:40 +0200

    Bridge Pattern Notifications

---
+++ Designs/Structural/bridge.ts
@@ -0,0 +1,85 @@
+/*======================================
+=            BRIDGE PATTERN            =
+======================================*/
+
+
+type Recipient = {email?: string, phone?: string, deviceId?: string}
+
+
+/*----------  Implementation  ----------*/
+
+class NotificationSender {
+
+	
+	send = ( message: string, recipient: Recipient ): string => {
+		throw new Error('The send method must be implemented')
+	}
+}
+
+export class EmailSender extends NotificationSender {
+
+
+	send = ( message: string, recipient: Recipient ): string => {
+		console.log( `Sending Email to ${recipient.email}: ${message}` )
+		return `Email sucessfully sent to ${recipient.email}`
+	}
+}
+
+
+export class SMSSender extends NotificationSender {
+
+	send = ( message: string, recipient: Recipient ): string => {
+		console.log( `Sending SMS to ${recipient.phone}: ${message}` )
+		return `SMS sucessfully sent to ${recipient.phone}`
+	}
+}
+
+
+export class PushSender extends NotificationSender {
+
+	send = ( message: string, recipient: Recipient ): string => {
+		console.log( `Sending push notification to ${recipient.deviceId}: ${message}` )
+		return `Push notification sucessfully sent to ${recipient.deviceId}`
+	}
+}
+
+
+/*----------  Abstraction  ----------*/
+
+
+class Notificator {
+
+	protected sender: NotificationSender
+
+	constructor ( sender: NotificationSender ) {
+		this.sender = sender
+	}
+
+	notify = ( message: string, recipient: Recipient ) => {
+		return this.sender.send(message, recipient)
+	}
+}
+
+export class NormalNotification extends Notificator {
+
+	constructor ( sender: NotificationSender ) {
+		super(sender)
+	}
+
+	notify = (message: string, recipient: Recipient) => {
+		const formattedMessage = `Normal : ${message}`
+		return this.sender.send(formattedMessage, recipient)
+	}
+}
+
+export class UrgentNotification extends Notificator {
+
+	constructor ( sender: NotificationSender ) {
+		super(sender)
+	}
+
+	notify = (message: string, recipient: Recipient) => {
+		const formattedMessage = `URGENT : ${message}`
+		return this.sender.send(formattedMessage, recipient)
+	}
+}
--- Designs/Structural/index.ts
+++ Designs/Structural/index.ts
@@ -1,10 +1,36 @@
+import { faker } from '@faker-js/faker'
 import { Calculator, CalculatorAdapter, NewCalculator } from "./adapter";
+import { EmailSender, NormalNotification, PushSender, SMSSender, UrgentNotification } from "./bridge";
 
+
+/*----------  Adapter  ----------*/
+
+console.log('Adapter')
 const old = new Calculator()
-console.log(old.operations(2, 2,'add'))
+console.log(old.operations(5, 2,'james'))
 
 const newC = new NewCalculator()
-console.log(newC.add(2, 2))
+console.log(newC.sub(5, 2))
 
 const adapter = new CalculatorAdapter()
-console.log(adapter.operations(2, 2,'add'))
+console.log(adapter.operations(5, 2,'div'))
+
+/*----------  Bridge  ----------*/
+
+const emailSender = new EmailSender()
+const smsSender = new SMSSender()
+const pushSender = new PushSender()
+
+const urgentEmail = new UrgentNotification(emailSender)
+const normalSMS = new NormalNotification(smsSender)
+const urgentPush = new UrgentNotification(pushSender)
+
+const recipient = {
+	email: faker.internet.email(),
+	phone: faker.phone.number(),
+	deviceId: faker.string.nanoid()
+}
+console.log('\nBridge')
+urgentEmail.notify(faker.git.commitMessage(), recipient)
+normalSMS.notify(faker.git.commitMessage(), recipient)
+urgentPush.notify(faker.git.commitMessage(), recipient)
